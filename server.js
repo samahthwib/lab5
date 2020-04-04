56a09fa185442c6bf9b8c8d2c22f9be9818656cd
samahthwib <samah.thweib@hotmail.com>
2020-04-04 18:09:53 +0300

    deployment1

--- server.js
+++ server.js
@@ -13,19 +13,3 @@ server.listen(PORT , ()=>{
 console.log('listening on port ', PORT);
 
 });
-
-// // http://localhost:3000/test
-// server.get('/test', (request, response) => {
-//     response.send('You are doing great');
-// });
-
-
-// // http://localhost:3000/data
-// server.get('/data',(request,response)=>{
-//     let family = [
-//         {name:"Atallah"},
-//         {name:"Messina"},
-//         {name:"Razan"}
-//     ];
-//     response.json(family);
-// })
