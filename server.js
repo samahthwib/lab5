acb1828be86fe40698cda8568c2cad9ce36f64d2
samahthwib <samah.thweib@hotmail.com>
2020-04-04 14:56:03 +0300

    edit

--- server.js
+++ server.js
@@ -7,3 +7,25 @@ const server = express();
 const PORT = process.env.PORT || 3000;
 
 server.use(express.static('./public'));
+
+server.listen(PORT , ()=>{
+
+console.log('listening on port ', PORT);
+
+});
+
+// http://localhost:3000/test
+server.get('/test', (request, response) => {
+    response.send('You are doing great');
+});
+
+
+// http://localhost:3000/data
+server.get('/data',(request,response)=>{
+    let family = [
+        {name:"Atallah"},
+        {name:"Messina"},
+        {name:"Razan"}
+    ];
+    response.json(family);
+})
